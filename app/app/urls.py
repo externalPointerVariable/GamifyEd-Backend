c255d3d21b957ff24869c562584b294d789f1269
externalPointerVariable <vicky986847@gmail.com>
2025-03-21 07:24:54 +0000

    Comment out unused root URL path in the URL configuration

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    # path('/'),
     path('admin/', admin.site.urls),
     path('favicon.ico',RedirectView.as_view(url='/static/favicon.ico', permanent = True)),
 ]
