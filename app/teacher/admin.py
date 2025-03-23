01cb2e318266e5d896d7c53963095f2f29faf0d9
externalPointerVariable <vicky986847@gmail.com>
2025-03-23 16:18:18 +0000

    Register TeacherProfile and Classrooms models in admin

--- app/teacher/admin.py
+++ app/teacher/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from teacher.models import TeacherProfile, Classrooms
 
 # Register your models here.
+admin.site.register(TeacherProfile)
+admin.site.register(Classrooms)
