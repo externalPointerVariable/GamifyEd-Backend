1027b500414406611a80e10354b9661cc57e3296
externalPointerVariable <vicky986847@gmail.com>
2025-03-25 15:15:40 +0000

    Add firstName and lastName fields to RegisterSerializer; update user creation logic for StudentProfile and TeacherProfile

--- app/student/serializers.py
+++ app/student/serializers.py
@@ -5,11 +5,13 @@ from teacher.models import TeacherProfile
 from rest_framework_simplejwt.tokens import RefreshToken
 
 class RegisterSerializer(serializers.ModelSerializer):
+    firstName = serializers.CharField()
+    lastName = serializers.CharField()
     role = serializers.ChoiceField(choices=UserProfile.ROLE_CHOICES, write_only=True)
     institution = serializers.CharField(write_only=True) 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'role', 'institution']
+        fields = ['username', 'firstName', 'lastName', 'email', 'password', 'role', 'institution']
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -17,12 +19,17 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         role = validated_data.pop('role')
         institution = validated_data.pop('institution', None)
+        firstName = validated_data.pop('firstName', None)
+        lastName = validated_data.pop('lastName', None)
         user = User.objects.create_user(**validated_data)
-        UserProfile.objects.create(user=user, role=role, institution=institution)
+        user.first_name = firstName
+        user.last_name = lastName
+        user.save()
+        UserProfile.objects.create(user=user.username, role=role, institution=institution)
         if role == "student":
-            StudentProfile.objects.create(user=user, email=user.email, institute=institution)
+            StudentProfile.objects.create(user=user.username, firstName=firstName, lastName=lastName, email=user.email, institute=institution)
         elif role == "teacher":
-            TeacherProfile.objects.create(user=user, email=user.email, institute=institution)
+            TeacherProfile.objects.create(user=user.username, firstName=firstName, lastName=lastName, email=user.email, institute=institution)
         return user
 
 
