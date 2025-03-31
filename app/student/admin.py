97189461df48d7d7fad99b62cf14ebae9c80b941
externalPointerVariable <vicky986847@gmail.com>
2025-03-31 11:22:51 +0000

    Register additional student-related models in the admin interface

--- app/student/admin.py
+++ app/student/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from student.models import UserProfile, StudentProfile, JoinedClassrooms
+from student.models import UserProfile, StudentProfile, JoinedClassrooms, StudentAIPodcast, StudentLoginStreak, StudentCalendarEvent, StudentTestHistory, AchievementsManagement, LevelHistory, LevelMilestones, LevelRewards, XPBreakdown, DailyMissions
 
 # Register your models here
 admin.site.register(UserProfile)
 admin.site.register(StudentProfile)
 admin.site.register(JoinedClassrooms)
+admin.site.register(StudentAIPodcast)
+admin.site.register(StudentLoginStreak)
+admin.site.register(StudentCalendarEvent)
+admin.site.register(StudentTestHistory)
+admin.site.register(AchievementsManagement)
+admin.site.register(LevelHistory)
+admin.site.register(LevelMilestones)
+admin.site.register(LevelRewards)
+admin.site.register(XPBreakdown)
+admin.site.register(DailyMissions)
